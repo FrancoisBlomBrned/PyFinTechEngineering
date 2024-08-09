163b21a10c81a0a5c473f9f0f235aad156117d21
jinxin.li@hotmail.com <jinxin.li@hotmail.com>
2024-08-08 21:25:47 -0400

    add some comment for rectifying plotting() inside 'ploting_func.py'. (for more info about the difference between keyword args and position args, refer to the website "https://www.geeksforgeeks.org/keyword-and-positional-argument-in-python/".) <😜simply demonstrating, there're some big diff between the call procedure of "plt.ylabel(y_label, fontsize = fontsize)" and that of "plt.ylabel(y_label,fontsize)">.
    the procedure of learning advanced tech of prog is sometimes exactly the process of learn how 2 dbg problems when prog running unexpectedly. 👀

--- Python_Analysis_of_Options/pkg0/ploting_func.py
+++ Python_Analysis_of_Options/pkg0/ploting_func.py
@@ -1,28 +1,90 @@
 #! "D:/PythonProjects/PyFinTechnology/PyFinTechEngineering/venv/scripts/python.exe"
 #_*_coding: UTF-8_*_
 
-def plotting(figsize, x_value, y_value, plotting_legend, line_label, width, x_label, y_label, fontsize, title):
-    # 导入所需的包以及修改字体
-    import matplotlib.pyplot as plt
-    from pylab import mpl
-    mpl.rcParams["font.sans-serif"] = ["FangSong"]
-    mpl.rcParams["axes.unicode_minus"] = False
-    from pandas.plotting import register_matplotlib_converters
-    register_matplotlib_converters()
+import traceback
+from typing import Tuple, Dict
 
-    # 绘图
-    # plt.figure(figsize)
-    plt.plot(x_value, y_value, plotting_legend, label = line_label, lw = width)
-    # TODO: plt.plot(S_list, value_approx1, "r-", label = u"运用Delta计算得到的看涨期权近似价格", lw = 2.5)
-    plt.xlabel(x_label, fontsize)
-    plt.ylabel(y_label, fontsize)
-    plt.xticks(fontsize)
-    plt.yticks(fontsize)
-    plt.title(title, fontsize)
-    plt.legend(fontsize)
-    plt.grid()
-    plt.show()
+def plotting_correct(figsize, x_value, y_value, plotting_legend, line_label, width, x_label, y_label, fontsize, title)\
+        -> Tuple[Dict, Dict]:
+
+    try:
+        # 导入所需的包以及修改字体
+        import matplotlib.pyplot as plt
+        from pylab import mpl
+        mpl.rcParams["font.sans-serif"] = ["FangSong"]
+        mpl.rcParams["axes.unicode_minus"] = False
+        from pandas.plotting import register_matplotlib_converters
+        register_matplotlib_converters()
+
+        # 绘图
+        # plt.figure(figsize)
+        plt.plot(x_value, y_value, plotting_legend, label = line_label, lw = width)
+        # TODO: plt.plot(S_list, value_approx1, "r-", label = u"运用Delta计算得到的看涨期权近似价格", lw = 2.5)
+        #########plt.xlabel(x_label, fontsize)
+        #########plt.ylabel(y_label, fontsize)
+
+        plt.xlabel(x_label, fontsize = fontsize)
+        plt.ylabel(y_label, fontsize = fontsize)
+
+        ########plt.xticks(fontsize)
+        ########plt.yticks(fontsize)
+        ########plt.title(title, fontsize)
+        ########plt.legend(fontsize)
+
+        plt.xticks(fontsize = fontsize)
+        plt.yticks(fontsize = fontsize)
+        plt.title(title, fontsize = fontsize)
+        plt.legend(fontsize = fontsize)
+
+    except ImportError as implE:
+        print(implE)
+    except BaseException as bsE:
+        print(bsE)
+    finally:
+        plt.grid()
+        plt.show()
+
+def plotting_error(figsize, x_value, y_value, plotting_legend, line_label, width, x_label, y_label, fontsize, title)\
+        -> None:
+
+    try:
+        # 导入所需的包以及修改字体
+        import matplotlib.pyplot as plt
+        from pylab import mpl
+        mpl.rcParams["font.sans-serif"] = ["FangSong"]
+        mpl.rcParams["axes.unicode_minus"] = False
+        from pandas.plotting import register_matplotlib_converters
+        register_matplotlib_converters()
+
+        # 绘图
+        # plt.figure(figsize)
+        plt.plot(x_value, y_value, plotting_legend, label = line_label, lw = width)
+        # TODO: plt.plot(S_list, value_approx1, "r-", label = u"运用Delta计算得到的看涨期权近似价格", lw = 2.5)
+        #########plt.xlabel(x_label, fontsize)
+        #########plt.ylabel(y_label, fontsize)
+        plt.xlabel(x_label, fontsize)
+        plt.ylabel(y_label, fontsize)
+        ############  65 行報錯
+####################  66 亦報錯
+
+        plt.xticks(fontsize)
+        plt.yticks(fontsize)
+        plt.title(title, fontsize)
+        plt.legend(fontsize)
+
+    except ImportError as implE:
+        print(implE)
+    except BaseException as bsE:
+        print(bsE)
+    finally:
+        plt.grid()
+        plt.show()
+
+
 
 if __name__ == "__main__":
-    plotting((9, 6), [1, 2, 3], [3, 4, 5], "r-", u"这是一个linelabel",
+    plotting_error((9, 6), [1, 2, 3], [3, 4, 5], "r-", u"这是一个linelabel",
              2.5, u"这是一个xlabel", u"这是一个ylabel", 13, u"这是一个title")
+
+    plotting_correct((9, 6), [1, 20, 35, 42.5, 45.5, 47.25, 49, 50.05, 50.53], [3, 4, 5, 6, 7, 8, 9, 10, 11], "r-", r"demo label",
+             2.5, r"look-xlabel", r"look-ylabel", 13, r"some title")
